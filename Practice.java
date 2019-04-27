1e95452a1d3d50143610038f57e92387cc19aabe
achint <achint.shrivastav@gmail.com>
2019-04-27 17:42:19 +0530

    first commit for first branch

---
+++ Practice.java
@@ -0,0 +1,23 @@
+class Practice 
+{
+public static void main(String[] args)
+{
+int a=10;
+a++;
+{
+System.out.println(a);
+a++;
+{
+System.out.println(a);
+++a;
+{
+System.out.println(a);
+++a;
+{
+System.out.println(a);
+}
+}
+}
+}
+}
+}
